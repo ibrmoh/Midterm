395f8c41b58e98b9626e4a2269bf9d52d6f3b288
ibrmoh <ibrahim1412nn@gmail.com>
2022-05-12 09:51:54 +0300

    midterm

--- resources/views/products/indix.blade.php
+++ resources/views/products/indix.blade.php
@@ -1,95 +1,42 @@
-@extends('layout.app')
-@section('content')
+@extends('Layout.app');
+@section('conect')
 
-
-<div class="container">
-    <div class="col-sm-offset-2 col-sm-8">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                New Task
-            </div>
-
-            <div class="panel-body">
-                <!-- Display Validation Errors -->
-                <!-- New Task Form -->
-                @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-                <form action="/task/store" method="POST" class="form-horizontal">
-                    @csrf
-
-                    <!-- Task Name -->
-                    <div class="form-group">
-                        <label for="task-name" class="col-sm-3 control-label">Task</label>
-
-                        <div class="col-sm-6">
-                            <input type="text" name="name" id="task-name" class="form-control" value="" >
-                        </div>
-                    </div>
-
-                    <!-- Add Task Button -->
-                    <div class="form-group">
-                        <div class="col-sm-offset-3 col-sm-6">
-                            <button type="submit" class="btn btn-default">
-                                <i class="fa fa-btn fa-plus"></i>Add Task
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4"><div style="position: absolute; inset: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;" class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div></div><div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h2">All Products</h1>
+            <div class="btn-toolbar mb-2 mb-md-0">
         </div>
+        </div>
+        <div class="btn-toolbar mb-2 mb-md-0">
+            <table class="table table-bordered">
+              <tbody>
+                  <tr>
+                      <th>iD</th>
+                      <th>name</th>
+                      <th>Price</th>
+                      <th>quantity</th>
+                      <th width="280px">Actions</th>
+                  </tr>
+                  <tr>
+                    @foreach ($products as $product )
 
-        <!-- Current Tasks -->
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    Current Tasks
-                </div>
+                      <td>{{$product->id}}</td>
+                      <td>{{$product->name}}</td>
+                      <td>{{$product->price}}</td>
+                      <td>{{$product->quantity}}</td>
+                      <td>
+                        <a class="btn btn-info" href="">Edit</a>
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                      </td>
+                  </tr>
+                  <tr>
+                    @endforeach
 
-                <div class="panel-body">
-                    <table class="table table-striped task-table">
-                        <thead>
-                            <th>No.</th>
-                            <th>Task</th>
-                            <th>&nbsp;</th>
-                        </thead>
-                        <tbody>
-                            @foreach ($tasks as $key => $task )
-                            <tr>
-                                <td class="table-text">{{++$key}}</td>
-                                <td class="table-text"><div> {{$task->name}}</div></td>
-
-                                <!-- Task Delete Button -->
-                                <td>
-                                    <form action="/task/destroy/{{$task->id}}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-danger">
-                                            <i class="fa fa-btn fa-trash"></i>Delete
-                                        </button>
-                                    </form>
-                                </td>
-                                <td>
-                                    <form action="{{ '/tasks/edit/' . $task->id }}" method="POST">
-                                        @csrf
-
-                                        <button type="submit" class="btn btn-primary ">
-                                            <i class="fa fa-pencil-square-o"></i>Eidt
-                                        </button>
-                                    </form>
-                                </td>
-                            </tr>
-                            @endforeach
-
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-    </div>
+              </tbody>
+            </table>
+        </div>
+    </main>
+  </div>
 </div>
+</body></html>
 @endsection
